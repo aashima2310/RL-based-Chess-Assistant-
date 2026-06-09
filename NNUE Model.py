229516c3fe63046893389d141533b5eb94b4089e
aashima2310 <aashimasingh2310@gmail.com>
2026-06-09 11:59:09 +0530

    made some changes in input size , added os to save it

--- NNUE Model.py
+++ NNUE Model.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
+import os
+
 class NNUE(nn.Module):
-    def __init__(self,input_size=41024):
+    def __init__(self,input_size=40960):
       super().__init__()
       self.input_weights=nn.Parameter(torch.randn(input_size,512)*0.01)
       self.input_bias=nn.Parameter(torch.zeros(512))
@@ -10,19 +12,24 @@ class NNUE(nn.Module):
       self.l4=nn.Linear(32,1)
     def clipped_relu(self,x):
         return torch.clamp(x,min=0,max=1)
-    def refresh_accumulator(self,active_features):
+    def refresh_accumulator(self,active_features : list)-> torch.Tensor:
         acc=self.input_bias.clone()
         for i in active_features:
           acc=acc+self.input_weights[i]
         return acc
-    def update_accumulator(self,accumulator,added_features,removed_features):
+    def update_accumulator(self,accumulator : torch.Tensor,added_features: list,removed_features: list) -> torch.Tensor:
         acc=accumulator.clone()
         for i in added_features:
           acc=acc+self.input_weights[i]
         for i in removed_features:
           acc=acc-self.input_weights[i]
         return acc
-    def forward(self, w_acc, b_acc):
+    def forward(self, w_acc : torch.Tensor, b_acc : torch.Tensor) -> torch.Tensor:
+        if w_acc.dim() == 1:
+            w_acc = w_acc.unsqueeze(0)
+        if b_acc.dim() == 1:
+            b_acc = b_acc.unsqueeze(0)
+            
         x = torch.cat([w_acc, b_acc], dim=1)
         
         x = self.clipped_relu(x)
@@ -30,3 +37,28 @@ class NNUE(nn.Module):
         x = self.clipped_relu(self.l3(x))
         x = self.l4(x)
         return x
+    def evaluate_board(
+        self,
+        board,
+        extractor
+    ) -> float:
+#returns centipawn score here.
+        self.eval()
+        with torch.no_grad():
+            w_idx = extractor.get_halfkp_indices(board, True)
+            b_idx = extractor.get_halfkp_indices(board, False)
+            w_acc = self.refresh_accumulator(w_idx)
+            b_acc = self.refresh_accumulator(b_idx)
+            score = self.forward(w_acc, b_acc)
+            return score.item()
+
+    def save(self, path: str):
+        os.makedirs(os.path.dirname(path) or ".", exist_ok=True)
+        torch.save(self.state_dict(), path)
+        print(f"NNUE saved to {path}")
+
+    def load_weights(self, path: str):
+        self.load_state_dict(
+            torch.load(path, map_location='cpu')
+        )
+        print(f"NNUE loaded from {path}")
