2c19ab4b6e68723dec440097cc4158b3163b20b6
aashima2310 <aashimasingh2310@gmail.com>
2026-06-14 13:24:10 +0530

    written the code

--- RL/RL_training/replay_buffer.py
+++ RL/RL_training/replay_buffer.py
@@ -1,1 +1,23 @@
+import random
+import numpy as np
+from collections import deque
+
+class ReplayBuffer:
+
+    def __init__(self, max_size=50000):
+        self.buffer = deque(maxlen=max_size)
+
+    def push(self, data):
+        self.buffer.extend(data)
+
+
+    def sample(self, batch_size):
+
+        batch = random.sample(self.buffer, batch_size)
+        states, policies, values = zip(*batch)
+
+        return states, np.array(policies), np.array(values, dtype=np.float32)
+
+    def __len__(self):
+        return len(self.buffer)
 
