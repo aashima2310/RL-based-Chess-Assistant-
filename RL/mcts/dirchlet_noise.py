19417d2e23c01bcb1dc7f03302c9dce3b75b917e
Akshay Pratap Singh <rathore.akshay789@gmail.com>
2026-06-14 16:24:22 +0530

    Update dirchlet_noise.py

--- RL/mcts/dirchlet_noise.py
+++ RL/mcts/dirchlet_noise.py
@@ -1,1 +1,3 @@
-
+def dirichlets_loss(policy:torch.Tensor,alpha: float=0.3,epsilon: float=0.25) -> torch.Tensor:
+  noise=torch.distributions.Dirichlet(torch.full_like(policy,alpha)).sample()
+  return (1-epsilon)*policy+epsilon*noise
