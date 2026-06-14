dab0420276d5897ecc3c752cc4903668dc6c4adc
Akshay Pratap Singh <rathore.akshay789@gmail.com>
2026-06-14 16:21:44 +0530

    Update node.py

--- RL/mcts/node.py
+++ RL/mcts/node.py
@@ -4,30 +4,30 @@ class Node:
     self.args = args
     self.state = state
     self.parent = parent
-    self.action_taken = action_taken 
+    self.action_taken = action_taken
 
     self.children = {}
     self.visit_count = 0
-    self.value_sum = 0 
-    self.policy = policy_from_nn 
+    self.value_sum = 0
+    self.policy = policy_from_nn
     self.valid_moves = game.get_valid_moves(state)
 
   def select(self):
     best_child = None
     best_ucb = -np.inf
 
-    
+
     for action in range(self.game.action_size):
-      if self.valid_moves[action] == 1: 
-        if action in self.children: 
+      if self.valid_moves[action] == 1:
+        if action in self.children:
           child = self.children[action]
           q_value = child.value_sum / child.visit_count if child.visit_count > 0 else 0
           if self.policy[action] == 0:
             ucb = -np.inf
           else:
             ucb = q_value + self.args['C'] * self.policy[action] * (math.sqrt(self.visit_count) / (1 + child.visit_count))
-        else: 
-          q_value = 0 
+        else:
+          q_value = 0
           if self.policy is None or self.policy[action] == 0:
             ucb = -np.inf
           else:
@@ -42,7 +42,7 @@ class Node:
       if len(legal_actions) > 0:
         return np.random.choice(legal_actions)
       else:
-        return None 
+        return None
 
     return best_child
 
