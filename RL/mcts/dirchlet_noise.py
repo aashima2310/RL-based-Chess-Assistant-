1e0d8bd949dc35209a2b480c897789ff4d2d7d51
Akshay Pratap Singh <rathore.akshay789@gmail.com>
2026-06-15 09:03:01 +0530

    Update dirchlet_noise.py

--- RL/mcts/dirchlet_noise.py
+++ RL/mcts/dirchlet_noise.py
@@ -5,6 +5,7 @@ import chess
 import torch
 import numpy as np
 from RL.chess_env.features import HalfKPExtractor
-halfkp_extractor = HalfKPExtractor()def dirichlets_loss(policy:torch.Tensor,alpha: float=0.3,epsilon: float=0.25) -> torch.Tensor:
+halfkp_extractor = HalfKPExtractor()
+def add_dirichlet_noise(policy:torch.Tensor,alpha: float=0.3,epsilon: float=0.25) -> torch.Tensor:
   noise=torch.distributions.Dirichlet(torch.full_like(policy,alpha)).sample()
   return (1-epsilon)*policy+epsilon*noise
