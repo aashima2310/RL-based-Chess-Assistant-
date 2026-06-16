1390d4c2923ee394659b071176c0ea03649d490f
Akshay Pratap Singh <rathore.akshay789@gmail.com>
2026-06-16 08:35:23 +0530

    Update trainer.py

--- RL/RL_training/trainer.py
+++ RL/RL_training/trainer.py
@@ -28,10 +28,14 @@ class Trainer:
         for epoch in range(Config.epochs):
 
             states, policies, values = replay_buffer.sample(Config.batch_size)
-            state_tensors = torch.stack([
-                board_to_tensor_769(s) for s in states
-            ])
-
+            w_accs, b_accs = [], []
+            for s in states:
+                w, b = halfkp_extractor.board_to_halfkp(s)
+                w_accs.append(w)
+                b_accs.append(b)
+            
+            w_acc = torch.stack(w_accs).to(self.device)
+            b_acc = torch.stack(b_accs).to(self.device)
             policy_targets = torch.tensor(policies, dtype=torch.float32)
             value_targets = torch.tensor(values, dtype=torch.float32).unsqueeze(1)
 
