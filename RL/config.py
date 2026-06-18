0095208150a5eac76327934c52a529c8b71b5710
Akshay Pratap Singh <rathore.akshay789@gmail.com>
2026-06-18 10:24:16 +0530

    Update config.py

--- RL/config.py
+++ RL/config.py
@@ -1,19 +1,18 @@
 class Config:
-    num_simulations = 100
+    num_simulations = 200
     c_puct = 1.5
 
     sim_schedule = {
-        0:  100,
-        5:  200,
-        15: 400,
-        25: 800
+        0:  200,
+        10:  400,
+        20: 800
     }
 
-    num_episodes = 100
-    temp_threshold = 10
-    batch_size = 512
-    lr = 2e-4
-    epochs = 10
+    num_episodes = 10
+    temp_threshold = 20
+    batch_size = 256
+    lr = 0.003
+    epochs = 20
     arena_games = 20
     update_threshold = 0.55
 
@@ -23,3 +22,4 @@ class Config:
     checkpoint_dir = "checkpoints/"
     log_path = "logs/training_log.csv"
     buffer_path = "buffer.pkl"
+    num_iterations=200
